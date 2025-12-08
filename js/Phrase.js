6b57ad8cea23afcc8eb6c70f001319acedbac696
Chris Watnee <chriswatnee@users.noreply.github.com>
2025-12-08 12:28:24 -0500

    Add addPhraseToDisplay function

--- js/Phrase.js
+++ js/Phrase.js
@@ -5,4 +5,17 @@ class Phrase {
   constructor(phrase) {
     this.phrase = phrase.toLowerCase();
   }
+
+  // Adds letter placeholders to the display
+  addPhraseToDisplay() {
+    const phraseUl = document.querySelector("#phrase ul");
+    const phraseArr = this.phrase.split("");
+    // Create phrase HTML string
+    const phraseHtml = phraseArr.map((letter) => {
+      const className = letter !== " " ? `hide letter ${letter}` : "space";
+      return `<li class="${className}">${letter}</li>`;
+    }).join("");
+    // Update phrase unordered list HTML
+    phraseUl.innerHTML = phraseHtml;
+  }
 }
