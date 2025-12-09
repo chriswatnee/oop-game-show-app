5bfe0bc68cf14d06b05f8336aba3cd85b13b7366
Chris Watnee <chriswatnee@users.noreply.github.com>
2025-12-09 12:28:55 -0500

    Add click event listener to qwerty div

--- js/app.js
+++ js/app.js
@@ -2,13 +2,24 @@
  * Project 4 - OOP Game App
  * app.js */
 
+// Stores Game object instance
+let game = null;
+
 // DOM element selections
 const startGameBtn = document.querySelector("#btn__reset");
+const qwertyDiv = document.querySelector("#qwerty");
 
 // Add a click event listener to the Start Game button
 startGameBtn.addEventListener("click", () => {
   // Create a new Game object
-  const game = new Game();
+  game = new Game();
   // Start the game
   game.startGame();
+});
+
+// Add click event listener to qwerty div
+qwertyDiv.addEventListener("click", (e) => {
+  if (e.target.tagName === "BUTTON") {
+    game.handleInteraction(e.target);
+  }
 });
