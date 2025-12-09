cbf9fdafd4c66ebeb190f238754877ffaee6fe9c
Chris Watnee <chriswatnee@users.noreply.github.com>
2025-12-09 12:16:16 -0500

    Add a click event listener to the Start Game button

--- js/app.js
+++ js/app.js
@@ -1,3 +1,14 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * app.js */
+
+// DOM element selections
+const startGameBtn = document.querySelector("#btn__reset");
+
+// Add a click event listener to the Start Game button
+startGameBtn.addEventListener("click", () => {
+  // Create a new Game object
+  const game = new Game();
+  // Start the game
+  game.startGame();
+});
