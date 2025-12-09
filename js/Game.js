ec49d5eb47c5ad832f5bbb88a4330a43adae3d6a
Chris Watnee <chriswatnee@users.noreply.github.com>
2025-12-09 08:53:50 -0500

    Create the Game Class with constructor

--- js/Game.js
+++ js/Game.js
@@ -1,3 +1,16 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Game.js */
+class Game {
+  constructor() {
+    this.missed = 0;
+    this.phrases = [
+      new Phrase("Around the clock"),
+      new Phrase("The early bird catches the worm"),
+      new Phrase("Two heads are better than one"),
+      new Phrase("Better safe than sorry"),
+      new Phrase("Practice makes perfect")
+    ];
+    this.activePhrase = null;
+  }
+}
